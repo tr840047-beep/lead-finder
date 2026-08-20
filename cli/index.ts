6ecad86c36370cfaf5f2f1ddf34809e95a75cdd4
tr840047-beep <tr840047@gmail.com>
2026-08-20 13:37:12 +0530

    feat: expose Nimbus CSV export command

--- cli/index.ts
+++ cli/index.ts
@@ -5,18 +5,14 @@ import { discoverCommand } from "./commands/discover";
 import { enrichCommand } from "./commands/enrich";
 import { statusCommand } from "./commands/status";
 import { nimbusCommand, nimbusScoreCommand } from "./commands/nimbus";
+import { nimbusExportCommand } from "./commands/nimbus-export";
 
 const program = new Command();
-
-program
-  .name("lead-finder")
-  .description("Apify-powered lead discovery and enrichment CLI")
-  .version("1.0.0");
-
+program.name("lead-finder").description("Apify-powered lead discovery and enrichment CLI").version("1.0.0");
 program.addCommand(discoverCommand);
 program.addCommand(enrichCommand);
 program.addCommand(statusCommand);
 program.addCommand(nimbusCommand);
 program.addCommand(nimbusScoreCommand);
-
+program.addCommand(nimbusExportCommand);
 program.parse();
