ab37c3eb05611e71c02bf42255bff79fdf5afe88
tr840047-beep <tr840047@gmail.com>
2026-08-20 14:58:23 +0530

    fix: lazy-load CLI commands to avoid server-only crash

--- cli/index.ts
+++ cli/index.ts
@@ -1,18 +1,52 @@
 #!/usr/bin/env node
 import "dotenv/config";
 import { Command } from "commander";
-import { discoverCommand } from "./commands/discover";
-import { enrichCommand } from "./commands/enrich";
-import { statusCommand } from "./commands/status";
-import { nimbusCommand, nimbusScoreCommand } from "./commands/nimbus";
-import { nimbusExportCommand } from "./commands/nimbus-export";
 
 const program = new Command();
-program.name("lead-finder").description("Apify-powered lead discovery and enrichment CLI").version("1.0.0");
-program.addCommand(discoverCommand);
-program.addCommand(enrichCommand);
-program.addCommand(statusCommand);
-program.addCommand(nimbusCommand);
-program.addCommand(nimbusScoreCommand);
-program.addCommand(nimbusExportCommand);
-program.parse();
+program
+  .name("lead-finder")
+  .description("Apify-powered lead discovery and enrichment CLI")
+  .version("1.0.0");
+
+const commandName = process.argv[2];
+
+switch (commandName) {
+  case "nimbus": {
+    const { nimbusCommand } = await import("./commands/nimbus");
+    program.addCommand(nimbusCommand);
+    break;
+  }
+  case "nimbus-score": {
+    const { nimbusScoreCommand } = await import("./commands/nimbus");
+    program.addCommand(nimbusScoreCommand);
+    break;
+  }
+  case "nimbus-export": {
+    const { nimbusExportCommand } = await import("./commands/nimbus-export");
+    program.addCommand(nimbusExportCommand);
+    break;
+  }
+  case "discover": {
+    const { discoverCommand } = await import("./commands/discover");
+    program.addCommand(discoverCommand);
+    break;
+  }
+  case "enrich": {
+    const { enrichCommand } = await import("./commands/enrich");
+    program.addCommand(enrichCommand);
+    break;
+  }
+  case "status": {
+    const { statusCommand } = await import("./commands/status");
+    program.addCommand(statusCommand);
+    break;
+  }
+  default: {
+    const { nimbusCommand, nimbusScoreCommand } = await import("./commands/nimbus");
+    program.addCommand(nimbusCommand);
+    program.addCommand(nimbusScoreCommand);
+    break;
+  }
+}
+
+await program.parseAsync();
