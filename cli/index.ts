b126711a8864a353359f50a15537833aaeeae7ca
tr840047-beep <tr840047@gmail.com>
2026-08-20 14:59:59 +0530

    fix: wrap CLI dynamic imports in async main for CJS tsx

--- cli/index.ts
+++ cli/index.ts
@@ -2,51 +2,58 @@
 import "dotenv/config";
 import { Command } from "commander";
 
-const program = new Command();
-program
-  .name("lead-finder")
-  .description("Apify-powered lead discovery and enrichment CLI")
-  .version("1.0.0");
+async function main() {
+  const program = new Command();
+  program
+    .name("lead-finder")
+    .description("Apify-powered lead discovery and enrichment CLI")
+    .version("1.0.0");
 
-const commandName = process.argv[2];
+  const commandName = process.argv[2];
 
-switch (commandName) {
-  case "nimbus": {
-    const { nimbusCommand } = await import("./commands/nimbus");
-    program.addCommand(nimbusCommand);
-    break;
+  switch (commandName) {
+    case "nimbus": {
+      const { nimbusCommand } = await import("./commands/nimbus");
+      program.addCommand(nimbusCommand);
+      break;
+    }
+    case "nimbus-score": {
+      const { nimbusScoreCommand } = await import("./commands/nimbus");
+      program.addCommand(nimbusScoreCommand);
+      break;
+    }
+    case "nimbus-export": {
+      const { nimbusExportCommand } = await import("./commands/nimbus-export");
+      program.addCommand(nimbusExportCommand);
+      break;
+    }
+    case "discover": {
+      const { discoverCommand } = await import("./commands/discover");
+      program.addCommand(discoverCommand);
+      break;
+    }
+    case "enrich": {
+      const { enrichCommand } = await import("./commands/enrich");
+      program.addCommand(enrichCommand);
+      break;
+    }
+    case "status": {
+      const { statusCommand } = await import("./commands/status");
+      program.addCommand(statusCommand);
+      break;
+    }
+    default: {
+      const { nimbusCommand, nimbusScoreCommand } = await import("./commands/nimbus");
+      program.addCommand(nimbusCommand);
+      program.addCommand(nimbusScoreCommand);
+      break;
+    }
   }
-  case "nimbus-score": {
-    const { nimbusScoreCommand } = await import("./commands/nimbus");
-    program.addCommand(nimbusScoreCommand);
-    break;
-  }
-  case "nimbus-export": {
-    const { nimbusExportCommand } = await import("./commands/nimbus-export");
-    program.addCommand(nimbusExportCommand);
-    break;
-  }
-  case "discover": {
-    const { discoverCommand } = await import("./commands/discover");
-    program.addCommand(discoverCommand);
-    break;
-  }
-  case "enrich": {
-    const { enrichCommand } = await import("./commands/enrich");
-    program.addCommand(enrichCommand);
-    break;
-  }
-  case "status": {
-    const { statusCommand } = await import("./commands/status");
-    program.addCommand(statusCommand);
-    break;
-  }
-  default: {
-    const { nimbusCommand, nimbusScoreCommand } = await import("./commands/nimbus");
-    program.addCommand(nimbusCommand);
-    program.addCommand(nimbusScoreCommand);
-    break;
-  }
+
+  await program.parseAsync();
 }
 
-await program.parseAsync();
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
